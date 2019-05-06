2984d76e93abc4f8ca036dc4cba4a6e1309e86c8
Aniket Telsinge <36655203+anikettelsinge@users.noreply.github.com>
2019-05-06 08:30:08 +0530

    Update Binary Search Tree : Lowest Common Ancestor.java
    Solution to Binary Search Tree : Lowest Common Ancestor problem in Problem Solving Section in Data Structures - Trees

--- Problem solving(Data-structures)/Binary Search Tree : Lowest Common Ancestor.java
+++ Problem solving(Data-structures)/Binary Search Tree : Lowest Common Ancestor.java
@@ -1,1 +1,32 @@
+Node res;
+static boolean v1Flag = false;
+static boolean v2Flag = false;
+static void check(Node root, int v1, int v2) {
+	if(root == null) return;
+	check(root.left, v1, v2);
 
+	if(root.data == v1) v1Flag = true;
+	if(root.data == v2) v2Flag = true;
+	if(v1Flag && v2Flag) return;
+	check(root.right, v1, v2);
+}
+public static Node lca(Node root, int v1, int v2) {
+	if(root == null) return null;
+	Queue<Node> queue = new ArrayDeque<>();
+	queue.add(root);
+	Node temp;
+	Node result = root;
+	while(!queue.isEmpty()) {
+		temp = queue.poll();
+		if(temp.left != null) queue.add(temp.left);
+		if(temp.right != null) queue.add(temp.right);
+		// System.out.print(temp.data+ " ");
+		v1Flag = false;
+		v2Flag = false;
+		check(temp, v1, v2);
+		if(v1Flag && v2Flag) result = temp;
+		// queue.remove();
+	}
+
+	return result;
+}
